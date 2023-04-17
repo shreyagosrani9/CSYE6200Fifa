a320129d913ee58c68c3f13bf7910099c4fc0921
vrushank0606 <98771098+vrushank0606@users.noreply.github.com>
2023-04-17 16:50:29 -0400

    Update Elements.java

--- src/main/java/fifa/Elements.java
+++ src/main/java/fifa/Elements.java
@@ -17,3 +17,22 @@ public class Elements {
         return sceneElements;
     }
 }
+package fifa;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javafx.scene.Node;
+
+public class Elements {
+
+    private Collection<Node> sceneElements = new LinkedList<Node>();
+
+    void add(Node node) {
+        sceneElements.add(node);
+    }
+
+    public Collection<Node> getElements() {
+        return sceneElements;
+    }
+}
