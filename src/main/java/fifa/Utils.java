2bc8f97701dfb77c9c66c31fc3fe8106ba1d6161
Kush Salaskar <90415338+Kush-Salaskar-Northeastern7@users.noreply.github.com>
2023-04-17 16:52:34 -0400

    Update Utils.java

--- src/main/java/fifa/Utils.java
+++ src/main/java/fifa/Utils.java
@@ -1,23 +1,31 @@
 package fifa;
 
 public class Utils {
-    
-    public Utils() { };
-    
-    public static Vector rotate(Vector vel, double angle) {
 
-        Vector rotatedVelocities = new Vector(0, 0);
+	// Empty constructor
+	public Utils() {
+	};
 
-        rotatedVelocities.x = vel.x * Math.cos(angle) - vel.y * Math.sin(angle);
-        rotatedVelocities.y = vel.x * Math.sin(angle) + vel.y * Math.cos(angle);
+	// Method to rotate a vector by a given angle
+	// Uses trigonometry functions (cos, sin) to perform rotation
+	// Returns a new vector with the rotated coordinates
+	public static Vector rotate(Vector vel, double angle) {
 
-        return rotatedVelocities;
-    }
+		Vector rotatedVelocities = new Vector(0, 0);
 
-    public static double getDistance(Vector pos1, Vector pos2) {
-        double xDistance = pos2.x - pos1.x;
-        double yDistance = pos2.y - pos1.y;
+		rotatedVelocities.xaxis = vel.xaxis * Math.cos(angle) - vel.yaxis * Math.sin(angle);
+		rotatedVelocities.yaxis = vel.xaxis * Math.sin(angle) + vel.yaxis * Math.cos(angle);
 
-        return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-    }
+		return rotatedVelocities;
+	}
+
+	// Method to calculate the Euclidean distance between two vectors (positions)
+	// Uses the distance formula (square root of sum of squares)
+	// Returns the calculated distance
+	public static double getDistance(Vector pos1, Vector pos2) {
+		double xDistance = pos2.xaxis - pos1.xaxis;
+		double yDistance = pos2.yaxis - pos1.yaxis;
+
+		return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+	}
 }
